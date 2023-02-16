ffc051ab7a55d8fff7ba109f4f2e2c671d6ae422
alisher <gseeghaerg@rthr>
2023-02-16 07:37:16 +0500

    js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var a = prompt ("Введите свое имя","")
+var b = +prompt ("Введите ваш год рождения","")
+var c = +prompt ("Введите нынешний год","")
+
+const obj ={ 
+    user:a,
+    age:c-b,
+    year_of_birth:b,
+    current_year:c,
+}
+
+console.log(obj)
+
+function name(params) {
+    alert(c-b)
+}
